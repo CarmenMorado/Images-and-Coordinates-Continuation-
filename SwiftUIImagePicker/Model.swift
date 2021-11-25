8c6dfb1bbb7ca829919f49a5cfe57c71efef492b
Carmen Morado <cmora171@fiu.edu>
2021-11-25 18:19:31 -0500

    Adding rectArray to the dictionary has been modified. Now there is a rectArray made for every image so that each picture's information is saved adequately.

--- SwiftUIImagePicker/Model.swift
+++ SwiftUIImagePicker/Model.swift
@@ -9,15 +9,16 @@
 import Foundation
 import SwiftUI
 
-class Model {
+struct Model {
     var currentImage: String
-    private var rectDict = [String: [Rect]]()
+    private(set) var rectDict = [String: [Rect]]()
+    //private(set) var rectArray = [Rect]()
     
     init(currentImage: String, imageName: String) {
         self.currentImage = currentImage
       }
     
-    func addImage(imageName: String) -> [Rect]{
+    mutating func addImage(imageName: String) -> [Rect]{
         if rectDict.keys.contains(imageName) {
             return rectDict[imageName]!
         }
@@ -28,4 +29,93 @@ class Model {
         }
     }
     
+    mutating func handleGesture(currentImage: String, value: DragGesture.Value) {
+        
+        let pointOfOrigin = (min(value.startLocation.x, value.location.x), min(value.startLocation.y, value.location.y))
+
+        let width = abs(value.location.x - value.startLocation.x)
+        
+        let height = abs(value.startLocation.y -  value.location.y)
+        
+        let rect = Rect(x: pointOfOrigin.0, y: pointOfOrigin.1, width: width, height: height)
+        
+        var rectArray = [Rect]()
+        
+        rectArray.append(rect)
+        
+        if (rectDict["\(currentImage)"]) == nil {
+            var rectArray = [Rect]()
+            rectArray.append(rect)
+            rectDict["\(currentImage)"] = rectArray
+        }
+        
+        else {
+            
+            rectDict["\(currentImage)"]?.append(contentsOf: rectArray)
+        }
+        
+        var jsonString = """
+        [
+            {
+        """
+
+        jsonString.append(makeJSON(dict: rectDict, arr:rectArray))
+        jsonString.append("""
+                ]
+            }
+        ]
+        """)
+       print(jsonString)
+        
+        if let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                                                            in: .userDomainMask).first {
+            let pathWithFilename = documentDirectory.appendingPathComponent("Essaie.json")
+            do {
+                try jsonString.write(to: pathWithFilename,
+                                     atomically: true,
+                                     encoding: .utf8)
+            } catch {
+                // Handle error
+            }
+        }
+        
+    }
+    
+    private func makeJSON(dict: Dictionary<String, [Rect]>, arr: Array<Rect>) -> String {
+        let size = arr.count
+        let last_element = arr[size-1]
+        var jsonString = ""
+        for (key, rects) in dict {
+            jsonString.append("\n        image: \(key),")
+            jsonString.append("\n")
+            jsonString.append("""
+            annotations: [
+                {
+
+    """)
+            
+            for rect in rects {
+                jsonString.append("                coordinates: { \n")
+                jsonString.append("                    x: \((rect.x).truncate(places: 2)), y: \((rect.y).truncate(places: 2)), width: \((rect.width).truncate(places: 2)), height: \((rect.height).truncate(places: 2))\n ")
+                jsonString.append("                }\n")
+                
+                if rect == last_element {
+                    jsonString.append("             }\n")
+                }
+                else {
+                    jsonString.append("             },\n")
+                }
+            }
+        }
+        
+        return jsonString
+    }
+    
+}
+
+
+extension CGFloat {
+    func truncate(places : CGFloat)-> CGFloat {
+        return CGFloat(floor(pow(10.0, CGFloat(places)) * self)/pow(10.0, CGFloat(places)))
+    }
 }
